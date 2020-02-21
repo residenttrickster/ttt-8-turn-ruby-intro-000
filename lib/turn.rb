d6ddc610632bb15f2bc0ab78142f65015546419e
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-21 00:51:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,13 +33,4 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  answer = gets.chomp
-  answer = input_to_index(answer)
-  if valid_move?(answer, board)
-  move(board, answer)
-  puts display_board(board)
-  else
-  puts "That is an invalid entry!"
-  turn(board)
-  end 
 end
