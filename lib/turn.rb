ad20db8d010dd3944eaf9e5cfecd1873c1ac0c7c
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-21 00:56:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,5 +32,12 @@ def move(board, index, character_player="X")
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
 end
