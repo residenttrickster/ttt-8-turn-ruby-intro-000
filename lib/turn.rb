2908c5ee6b6213892a919423017700515654c87c
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-21 00:37:03 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move? (board, index)
+  if position_taken?(board,index) || index < 0 || index > 8
+    return false
+  elsif !position_taken?(board, index)
+    return true
+  end
+end
+
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def turn
+end
