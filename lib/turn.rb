f634f949cd5b1bbbe82782d7a417f894bb953744
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-21 01:19:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
 puts "Please enter 1-9:"
 input = gets.chomp
 input = input_to_index(input)
-if valid_move?(board, index)
+if valid_move?
 move(board, index, character_player="X")
 puts display_board(board)
   end
